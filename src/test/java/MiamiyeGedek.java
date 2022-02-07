89c8664ab22995314794feab0ba939c0e885d3a7
Irashad <reshad.nesirov5@gmail.com>
2022-02-07 19:23:22 +0400

    -okdyu

---
+++ src/test/java/MiamiyeGedek.java
@@ -0,0 +1,25 @@
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MiamiyeGedek {
+
+    WebDriver driver;
+    @BeforeTest
+    public void smsgondergelirem()
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\resha\\IdeaProjects\\jeko\\Driver\\chromedriver.exe");
+       driver = new ChromeDriver();
+    }
+
+    @Test
+    public void hardaQaldinDeGorum()
+    {
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+    }
+
+
+}
